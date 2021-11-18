8c9dde1a1d71a815dfe95fd1e261a9bcb0905416
gesora96 <nahidahmed9933@gmail.com>
2021-11-18 23:54:36 +0600

    install packages :thumbsup:

---
+++ src/pages/DashBoardPage/AddPlace/AddPlace.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddPlace = () => {
+  return <div></div>;
+};
+
+export default AddPlace;
---
+++ src/pages/DashBoardPage/UserOrder/UserOrder.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserOrder = () => {
+  return <div></div>;
+};
+
+export default UserOrder;
